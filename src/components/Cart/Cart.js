b42edffc762598157847f72eeee75b7cb70bc781
Keith Powell <keith@tapcart.co>
2022-04-02 11:04:57 -0700

    Dynamically rendering cart items in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-content";
 import Modal from "../UI/Modal/Modal";
+import CartItem from "./CartItem/CartItem";
 
 import styles from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const cartItemAddHandler = item => {}
+
+  const cartItemRemoveHandler = id => {}
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: "2", price: 12.99 }].map((item) => {
-        return <li key={item.id}>{item.name}</li>;
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        );
       })}
     </ul>
   );
@@ -18,11 +36,15 @@ const Cart = (props) => {
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
-        <span>35.00</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles["button--alt"]} onClick={props.onButtonClick}>Close</button>
-        <button className={styles.button}>Order</button>
+        <button className={styles["button--alt"]} onClick={props.onButtonClick}>
+          Close
+        </button>
+        {cartCtx.items.length > 0 ? (
+          <button className={styles.button}>Order</button>
+        ) : null}
       </div>
     </Modal>
   );
